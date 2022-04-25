67bf06a95a6e53707246d1180e766cfca5e1c1c3
[bonface221] <[mainaboniface221@gmail.com]>
2022-04-25 17:12:06 +0300

    ft:added the class to read the txt

--- readfiles.py
+++ readfiles.py
@@ -1,10 +1,12 @@
+
 text_file = 'text.txt'
-def read_file(text_file):
-    try:   
-        with open(text_file, 'r') as handle:
+
+
+def read_file(file):
+    try:
+        with open(file, 'r', encoding='utf_8') as handle:
             data = handle.read()
             return data
-        
+
     except FileNotFoundError:
         return None
-
